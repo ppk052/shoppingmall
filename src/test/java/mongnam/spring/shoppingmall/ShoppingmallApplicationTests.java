2b41f28ff41d9265d1ebc0e067ba4ad94d16a257
MongNam <ppk052@naver.com>
2024-11-08 17:47:37 +0900

    first commit

---
+++ src/test/java/mongnam/spring/shoppingmall/ShoppingmallApplicationTests.java
@@ -0,0 +1,13 @@
+package mongnam.spring.shoppingmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingmallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
